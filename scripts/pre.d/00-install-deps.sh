e33ba416bf48ad380a3162c3c22389b67b507b94
root <root@app01.cwalsh-v1p1.c4cloud.co.uk>
2012-03-13 14:38:32 +0000

    add nginx latest kinda role

--- scripts/pre.d/00-install-deps.sh
+++ scripts/pre.d/00-install-deps.sh
@@ -2,7 +2,12 @@
 
 # Sample file that installs your dependancies on launch
 
+echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu lucid main" > /etc/apt/sources.list.d/nginx-stable-lucid.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C
+
+
 apt-get update
+apt-get install -y nginx 
 #install mysql and postgres libs (these are a bugger to install via pip)
 apt-get install -y python-mysqldb python-psycopg2 
 #install django from standard repo (This can be overriden if install in virtual-environment)
